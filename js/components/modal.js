3ff5edc60e788f6316bcd1c3411a9802f0ea9e9c
Miguel Bermell <miguelbermell@gmail.com>
2021-05-09 21:10:28 +0200

    TodoList

---
+++ js/components/modal.js
@@ -0,0 +1,18 @@
+export default class Modal {
+  constructor() {
+    this.title = document.getElementById('modal-title');
+    this.description = document.getElementById('modal-description');
+    this.btn = document.getElementById('modal-btn');
+    this.completed = document.getElementById('modal-completed');
+  }
+
+  onClick(callback) {
+    this.btn.onclick = () => {
+      if(title.value === '' || description.value === '') {
+        this.alert.show('Title and description are required');
+        return;
+      }
+      $('#myModal').modal('toggle');
+    }
+  }
+}
